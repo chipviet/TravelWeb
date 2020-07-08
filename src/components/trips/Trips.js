e5f82a331faf926cf6f86bf44aaca1e1ec877bbc
tuanconbu <hieutk5@gmail.com>
2020-07-08 17:57:51 +0700

    update gridItem

--- src/components/trips/Trips.js
+++ src/components/trips/Trips.js
@@ -1,5 +1,6 @@
 import './tripStyle.css'
 import React from 'react'
+import GridList from '@material-ui/core/GridList';
 import image from '../../assets/thanhhoa.jpg'
 export default function Trips() {
     return (
@@ -18,29 +19,31 @@ export default function Trips() {
                 </div>
             </div>
             <div className="list-item">
-                <div className="item">
-                    <div className="place-image" >
-                        <img src={image} width="200px" height="250px"></img>
-                    </div>
-                    <div className="place-name">
-                        <p><b>Thanh Hoa, Vietnam</b></p>
-                    </div>
-                    <div className="place-rating-price">
-                        <div className="place-rating">
-                            <span class="fa fa-star checked"></span>
-                            <span class="fa fa-star checked"></span>
-                            <span class="fa fa-star checked"></span>
-                            <span class="fa fa-star"></span>
-                            <span class="fa fa-star"></span>
+                <GridList cellHeight={500} cols={5}>
+                    <div className="item">
+                        <div className="place-image" >
+                            <img src={image} width="200px" height="250px"></img>
                         </div>
-                        <div className="place-price">
-                            <p>$200 </p>
+                        <div className="place-name">
+                            <p><b>Thanh Hoa, Vietnam</b></p>
+                        </div>
+                        <div className="place-rating-price">
+                            <div className="place-rating">
+                                <span class="fa fa-star checked"></span>
+                                <span class="fa fa-star checked"></span>
+                                <span class="fa fa-star checked"></span>
+                                <span class="fa fa-star"></span>
+                                <span class="fa fa-star"></span>
+                            </div>
+                            <div className="place-price">
+                                <p>$200 </p>
+                            </div>
+                        </div>
+                        <div className="place-description">
+                            <p>Lorem ipsum dolor Lorem ipsum dolorLorem ipsum dolorLorem ipsum dolorLorem ipsum dolor Lorem</p>
                         </div>
                     </div>
-                    <div className="place-description">
-                        <p>Lorem ipsum dolor Lorem ipsum dolorLorem ipsum dolorLorem ipsum dolorLorem ipsum dolor Lorem</p>
-                    </div>
-                </div>
+                </GridList>
             </div>
         </div>
     )
