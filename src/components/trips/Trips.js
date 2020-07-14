3773017d027abc627d60982053972282b2b3e200
tuanconbu <hieutk5@gmail.com>
2020-07-14 22:12:01 +0700

    update cc

--- src/components/trips/Trips.js
+++ src/components/trips/Trips.js
@@ -6,17 +6,41 @@ import image from '../../assets/thanhhoa.jpg'
 import { render } from '@testing-library/react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
-import {getAllPlace} from '../../redux/actions/places';
-import {getAllPlaces} from '../../selectors/place';
- class Trips extends Component{
-    
-    componentDidMount(){
+import { getAllPlace } from '../../redux/actions/places';
+import { getAllPlaces } from '../../selectors/place';
+class Trips extends Component {
+    componentDidMount() {
         this.props.getAllPlace()
     }
-
-    render(){
-        const {data} = this.props;
-        console.log('data',data);
+    render() {
+        const { data } = this.props;
+        const listItem = data.map((item, index) =>
+            <Link style={{ textDecoration: 'none', color: 'black' }} to="/details">
+                <div className="item" key={index}>
+                    <div className="place-image" >
+                        <img src={image} alt='' width="200px" height="250px"></img>
+                    </div>
+                    <div className="place-name">
+                        <p><b>{item.Name}</b></p>
+                    </div>
+                    <div className="place-rating-price">
+                        <div className="place-rating">
+                            <span class="fa fa-star checked"></span>
+                            <span class="fa fa-star checked"></span>
+                            <span class="fa fa-star checked"></span>
+                            <span class="fa fa-star"></span>
+                            <span class="fa fa-star"></span>
+                        </div>
+                        <div className="place-price">
+                            <p>$200 </p>
+                        </div>
+                    </div>
+                    <div className="place-description">
+                        <p>{item.description}</p>
+                    </div>
+                </div>
+            </Link>
+        )
         return (
             <div className="main-content">
                 <div className="header-content">
@@ -33,50 +57,25 @@ import {getAllPlaces} from '../../selectors/place';
                     </div>
                 </div>
                 <div className="list-item">
-                    {data ?  ( <GridList cellHeight={500} cols={5}>
-                        <Link style={{ textDecoration: 'none', color: 'black' }} to="/details">
-                            <div className="item">
-                                <div className="place-image" >
-                                    <img src={image} alt='' width="200px" height="250px"></img>
-                                </div>
-                                <div className="place-name">
-                                    <p><b>{data[0].Name}</b></p>
-                                </div>
-                                <div className="place-rating-price">
-                                    <div className="place-rating">
-                                        <span class="fa fa-star checked"></span>
-                                        <span class="fa fa-star checked"></span>
-                                        <span class="fa fa-star checked"></span>
-                                        <span class="fa fa-star"></span>
-                                        <span class="fa fa-star"></span>
-                                    </div>
-                                    <div className="place-price">
-                                        <p>$200 </p>
-                                    </div>
-                                </div>
-                                <div className="place-description">
-                                    {/* <p>{data[1].description}</p> */}
-                                </div>
-                            </div>
-                        </Link>
-    
-                    </GridList>): (null)}
-                   
+                    {data ? (<GridList cellHeight={500} cols={5}>
+                        {listItem}
+                    </GridList>) : (null)}
+
                 </div>
             </div>
         )
     }
-    
-    }
-    
+
+}
+
 export default connect(
     state => ({
-      data: getAllPlaces(state),
+        data: getAllPlaces(state),
     }),
-    dispatch => 
-      bindActionCreators(
-        {
-          getAllPlace
-        }, dispatch
-      )
-  ) (Trips);
+    dispatch =>
+        bindActionCreators(
+            {
+                getAllPlace
+            }, dispatch
+        )
+)(Trips);
